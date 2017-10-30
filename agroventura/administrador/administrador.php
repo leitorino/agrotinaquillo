50486e6b8b8a50c920113f9dc097fb2476f87984
andreas6440 <33234042+andreas6440@users.noreply.github.com>
2017-10-30 19:18:36 -0400

    admin
    cambios en administrador.php

--- agroventura/administrador/administrador.php
+++ agroventura/administrador/administrador.php
@@ -2,15 +2,15 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	
+
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	
+
 	<meta name="description" content="">
-	
+
 	<meta name="author" content="">
-	
+
 	<link rel="icon" href="../foto/logo_agro.png">
-	
+
 	<title>Trabajador</title>
 
     <!-- Bootstrap core CSS -->
@@ -19,45 +19,46 @@
      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <!--Fonts-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-    
+
     <!-- Icons -->
     <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    
+
     <!-- Custom styles for this template -->
     <link href="../css/kkk.css" rel="stylesheet">
-    
+
 </head>
 <body>
 	<div class="container-fluid" id="wrapper">
 		<div class="row">
-			
+
 			<nav class="sidebar col-xs-12 col-sm-4 col-lg-3 col-xl-2 bg-faded sidebar-style-1 titulo  " >
 				<h1 class="site-title"><a href="index.html"> Agroaventuras CA</a></h1>
-				
+
 				<a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><em class="fa fa-bars"></em></a>
-				
+
 				<ul class="nav nav-pills flex-column sidebar-nav">
-					<li class="nav-item my-3"><a class="nav-link" href="producto.php" ><em class="fa fa-dashboard"></em> Control de productos</a></li>
+					<li class="nav-item my-3"><a class="nav-link" href="producto.php" ><em class="fa fa-cutlery"></em> Control de productos</a></li>
+					<li class="nav-item my-3"><a class="nav-link active" href="#trabajador" role="tab" data-toggle="tab"><em class="fa fa-user"></em> Trabajador </a></li>
 					<li class="nav-item my-3"><a class="nav-link" href="entrada.php" ><em class="fa fa-calendar-o"></em> Control de entradas </a></li>
-					<li class="nav-item my-3"><a class="nav-link" href="caney.php"><em class="fa fa-bar-chart"></em> Control Caney </a></li>
-					
-					<li class="nav-item my-3"><a class="nav-link active" href="#trabajador" role="tab" data-toggle="tab"><em class="fa fa-clone"></em> Trabajador  <span class="sr-only">(current)</span></a></li>
+					<li class="nav-item my-3"><a class="nav-link" href="caney.php"><em class="fa  fa-home "></em> Control Caney </a></li>
+
+
 				</ul>
-				
+
 				</nav>
-			
+
 	    <div class="main-panel col-xs-12 col-sm-8 offset-sm-4 col-lg-9 offset-lg-3 col-xl-10 offset-xl-2 pt-3 pl-4 ml-auto ">
-            
+
     <div id="nuevo-trabajador"class="modal fade " tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
-      <div class="modal-header ml-auto" > 
-       
-    
+      <div class="modal-header ml-auto" >
+
+
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-       
+
       </div>
       <div class="modal-body m-0 p-0">
        <section class="my-3">
@@ -72,66 +73,67 @@
 									<p class="category">Registro nuevo trabajador</p>
 	                            </div>
 	                            <div class="card-content">
-	                                <form>
+	                                <form method="post">
 	                                    <div class="row my-3">
-	                                        
+
 	                                        <div class="col-md-5">
 												<div class="form-group group ">
-													<input type="text" class="input2" id="usuario-r" required>
+													<input type="text" class="input2 val-letras" id="usuario-r"  maxlength="25" required>
                                                       <span class="highlight"></span>
                                                       <span class="bar"></span>
                                                       <label class="label2">Nombre de usuario</label>
-                                                      
+
 												</div>
 	                                        </div>
 	                                       <div class="col-md-3">
 												<div class="form-group group ">
-													<input type="email" class="input2" id="correo-r"  required>
+													<input type="email" class="input2" id="correo-r" maxlength="60"  required>
                                                       <span class="highlight"></span>
                                                       <span class="bar"></span>
                                                       <label class="label2">Correo</label>
-                                                      
+
 												</div>
 	                                        </div>
-	                                       
-	                                        
+
+
 	                                        <div class="col-md-4">
 												<div class="form-group group ">
-													<input type="email" class="input2" id="telefono-r" required>
+													<input type="text" class="input2  val-numero"  id="telefono-r" maxlength="20" required >
                                                       <span class="highlight"></span>
                                                       <span class="bar"></span>
                                                       <label class="label2">teléfono</label>
-                                                      
+
+
 												</div>
 	                                        </div>
 	                                    </div>
                                         <div class="row my-5">
                                         <div class="col-md-5">
 												<div class="form-group group ">
-													<input type="password" class="input2" id="contraseña-r" required>
+													<input type="password" class="input2" id="contraseña-r" minlength="8" maxlength="49" required>
                                                       <span class="highlight"></span>
                                                       <span class="bar"></span>
                                                       <label class="label2">Contraseña</label>
-                                                      
+
 												</div>
 	                                        </div>
-	                                       
+
 	                                        <div class="col-md-3">
 												<div class="form-group group ">
-													<input type="text" class="input2" id="codigo-r" required>
+													<input type="text" class="input2 val-numero" id="codigo-r" maxlength="10" required>
                                                       <span class="highlight"></span>
                                                       <span class="bar"></span>
                                                       <label class="label2">codigo</label>
-                                                      
+
 												</div>
 	                                        </div>
 	                                        <div class="col-md-4 my-2">
 												<div class="form-group  group">
-                                               
+
                                                 <select class="form-control input2" id="estado-r">
                                                   <option>Activo</option>
                                                   <option>Inactivo</option>
-                                                  
+
                                                 </select>
                                                 <span class="highlight"></span>
                                                 <span class="bar"></span>
@@ -142,9 +144,9 @@
                                          <div class="row">
 	                                        <div class="col-md-6 my-2">
 												<div class="form-group group">
-                                               
+
                                                 <select class="input2" id="t-documento-r">
-                                                  <option>Cedula</option>
+                                                  <option>Cédula</option>
                                                   <option>Rif</option>
                                                   <option>Pasaporte</option>
                                                 </select>
@@ -153,58 +155,58 @@
                                                  <label class="label2">Tipo de documeto</label>
                                               </div>
 	                                        </div>
-	                                       
+
 	                                        <div class="col-md-6">
 												<div class="form-group group ">
-													<input type="text" class="input2" id="n-documento-r" required>
+													<input type="text" class="input2 val-numero" id="n-documento-r" maxlength="25" required>
                                                       <span class="highlight"></span>
                                                       <span class="bar"></span>
                                                       <label class="label2">Numero de documento</label>
-                                                      
+
 												</div>
 	                                        </div>
 	                                    </div>
 	                                    <div class="row my-5">
-	                                       
+
 	                                        <div class="col-md-6">
 												<div class="form-group group ">
-													<input type="text" class="input2" id="nombre-r" required>
+													<input type="text" class="input2" id="nombre-r" maxlength="25" required>
                                                       <span class="highlight"></span>
                                                       <span class="bar"></span>
                                                       <label class="label2">Nombre</label>
-                                                      
+
 												</div>
 	                                        </div>
-	                                        
+
 	                                        <div class="col-md-6">
 												<div class="form-group group ">
-													<input type="test" class="input2" id="apellidos-r" required>
+													<input type="test" class="input2" id="apellidos-r" maxlength="25" required>
                                                       <span class="highlight"></span>
                                                       <span class="bar"></span>
                                                       <label class="label2">Apellidos</label>
-                                                      
+
 												</div>
 	                                        </div>
 	                                    </div>
 
 	                                    <div class="row">
-	                                        
+
 	                                        <div class="col-md-8">
 												<div class="form-group group ">
-													<input type="text" class="input2" id="direccion-r" required>
+													<input type="text" class="input2" id="direccion-r" maxlength="80" required>
                                                       <span class="highlight"></span>
                                                       <span class="bar"></span>
                                                       <label class="label2">Dirección</label>
-                                                      
+
 												</div>
 	                                        </div>
 	                                         <div class="col-md-4 my-2">
 												<div class="form-group group">
-                                                
+
                                                 <select class="input2" id="estado-r">
                                                   <option>vendedor</option>
                                                   <option>administrador</option>
-                                                  
+
                                                 </select>
                                                  <span class="highlight"></span>
                                                       <span class="bar"></span>
@@ -212,7 +214,7 @@
                                               </div>
                                              </div>
 	                                    </div>
-	                                   
+
 
 	                                    <button id="registrar" type="submit" class="btn  pull-right color-verde titulo">Registrar</button>
 	                                    <div class="clearfix"></div>
@@ -220,7 +222,7 @@
 	                            </div>
 	                        </div>
 	                    </div>
-						
+
 	                </div>
 	            </div>
 	        </div>
@@ -245,8 +247,8 @@
 	                             <div class="card-content table-responsive">
 	                                <table class="table" id="tabla-trabajador">
 	                                    <thead class="text-primary">
-	                                        
-	                                    	<th id="check"></th>
+
+
 	                                    	<th id="id">ID</th>
 	                                    	<th id="usuario-tab">Nombre de usuario</th>
 	                                    	<th id="nombre-tab">nombre</th>
@@ -258,12 +260,7 @@
 	                                    </thead>
 	                                    <tbody><div>
 	                                        <tr>
-	                                            <td>
-                                                  <label>
-                                                    <input type="checkbox" class="option-input checkbox" checked />
-                                                    
-                                                  </label>
-                                                </td>
+
                                                 <td>01</td>
 	                                        	<td>Andrea6440</td>
 	                                        	<td>andrea</td>
@@ -278,12 +275,7 @@
 														</td>
 	                                        </tr>
 	                                        <tr>
-	                                            <td>
-	                                            <label>
-                                                <input type="checkbox" class="option-input checkbox" />
-                                            
-                                              </label>
-                                            </td>
+
                                                  <td>02</td>
 	                                        	<td>maria124</td>
 	                                        	<td>maria</td>
@@ -300,29 +292,43 @@
 	                                       </div>
 	                                    </tbody>
 	                                </table>
-	                                 
-	                                 <button type="button" class="btn  pull-right color-verde titulo" data-toggle="modal" data-target="#nuevo-trabajador">Nuevo</button>
+
+	                                 <div class="row my-3">
+                                        <div class="col-md-5"></div>
+	                                    <div class="col-md-2">
+												<div class="form-group group  ">
+													<input type="search" class="input2 val-letras fa fa-search" id="busqueda"  maxlength="25" placeholder="realiza tu busqueda aqui…" >
+                                                      <span class="highlight"></span>
+                                                      <span class="bar"></span>
+
+                                                     <center><i class="fa fa-search" aria-hidden="true"></i></center>
+												</div>
+	                                        </div>
+	                                        <div class="col-md-3 ml-auto">
+												<div class="form-group group ">
+													<button type="button" class="btn  pull-right color-verde titulo" data-toggle="modal" data-target="#nuevo-trabajador">Nuevo</button>
+
+												</div>
+	                                        </div>
+	                                </div>
 
 
-
-                                 
-                                  
 	                            </div>
 	                        </div>
 	                    </div>
-						
+
 	                </div>
 	            </div>
 	        </div>
 </section>
-       
-      
-     
-        
-	        <footer class="footer p-0">
-        
-	            <div class="container-fluid">
-	                <nav class="pull-left">
+
+
+
+
+	        <footer class="footer p-0  " >
+
+	            <div class="container-fluid   ">
+	                <nav class="pull-left ">
 	                    <ul>
 	                        <li>
 	                            <a href="../index.php">
@@ -334,16 +340,16 @@
 	                                Cerrar Sesión
 	                            </a>
 	                        </li>
-	                        
+
 	                    </ul>
 	                </nav>
 	                <p class="copyright pull-right">
 	                    &copy; <a href="#">Agroaventuras C.A.</a>
 	                </p>
-	              
+
 	            </div>
 	        </footer>
-	        
+
 	    </div>
 		</div>
 	</div>
@@ -354,12 +360,12 @@
     <script src="../vendor/jquery/jquery.min.js"></script>
     <script src="../vendor/popper/popper.min.js"></script>
     <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-    
-    
+
+
     <script src="../js/bootstrap-datepicker.js"></script>
     <script src="../js/custom.js"></script>
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    
+
 	  </body>
 </html>
--- agroventura/js/custom.js
+++ agroventura/js/custom.js
@@ -1,4 +1,4 @@
-$('#nuevo-trabajador').appendTo("body") 	
+$('#nuevo-trabajador').appendTo("body")
 
 $("#menu-toggle").click(function(e) {
         e.preventDefault();
@@ -10,4 +10,14 @@ $('.f-date').datepicker({
     language: "es"
 });
 
-/*formulario*/
+$('.val-letras').keydown(function (e) {
+                var tecla = document.all ? tecla = e.keyCode : tecla = e.which;
+                return !((tecla > 47 && tecla < 58) || tecla == 46);
+            });
+
+    $(".val-numero").keydown(function(event){
+
+        if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105) && event.keyCode !==190  && event.keyCode !==110 && event.keyCode !==8 && event.keyCode !==9  ){
+            return false;
+        }
+    });
