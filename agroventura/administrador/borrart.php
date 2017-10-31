374a6c4fd2d1737c1ad77b560c9846f4e5b33ad5
leitorino <leitorino_ajl2@hotmail.com>
2017-10-31 12:40:37 -0400

    borrar

--- agroventura/administrador/borrart.php
+++ agroventura/administrador/borrart.php
@@ -1,12 +1,12 @@
 <?php
 
-include("bd/conexion.php");
+include("../bd/conexion.php");
 error_reporting(0);
 $id = $_GET['id'];
 
 $sql= "DELETE FROM `trabajor` WHERE `idpersona` = $id";
 $result = mysqli_query($link, $sql);
 
-header("Location: administrador/administrador.php");
+header("Location: administrador.php");
 
 ?>
