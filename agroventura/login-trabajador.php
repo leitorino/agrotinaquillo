9995e49b7e99140302bf91806e229686baf9ef67
leitorino <leitorino_ajl2@hotmail.com>
2017-10-30 23:05:24 -0400

    php para loggear

---
+++ agroventura/login-trabajador.php
@@ -0,0 +1,30 @@
+          <?php 
+
+            include("bd/conexion.php");
+
+                if(isset($_POST['usuario'])){
+                 $usuario =$_POST["usuario"];
+                }else{
+                    $usuario="";
+                }
+
+                if(isset($_POST['clave'])){
+                 $pass =$_POST['clave'];
+                }else{
+                    $pass="";
+                }   
+
+            $sql= "SELECT * FROM `persona` WHERE `usuario` = '$usuario'";
+            $result = mysqli_query($link, $sql);
+            $row = mysqli_fetch_array($result);
+            $comp=$row[2];
+
+            if ($pass == $comp){
+                session_start();
+                $_SESSION["usuariot"]= $_POST["usuario"];
+                //header('Location: usuario/usuario.php');
+
+            }else{ 
+                header('Location: index.php?el=1');
+            }
+        ?>
